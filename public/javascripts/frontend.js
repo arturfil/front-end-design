b3913b89662d84c4d628ff5b0f117ad6cb2504a7
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-09-10 16:34:16 -0400

    added first media querys

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -30,19 +30,15 @@ $(document).ready(function(){
       })
     }
   });
-
-
-
 });
 
 //add smooth scrolling
 $(document).ready(function() {
   'use strict';
 
-  $('.nav-item, #scroll-to-top')
+  $('.nav-item')
   // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
+
   .click(function(event) {
     // On-page links
     if (
@@ -74,4 +70,47 @@ $(document).ready(function() {
       }
     }
   });
+});
+
+// active menu on click
+$(document).ready(function(){
+  'use strict';
+  $('.navbar-nav li a').click(function(){
+    'use strict';
+    $('.navbar-nav li a').parent().removeClass('active');
+    $(this).parent().addClass('active');
+  })
+});
+
+$(document).ready(function(){
+  'use strict';
+  $(window).scroll(function(){
+    'use strict';
+    $('section').each(function(){
+      'use strict';
+      var bb = $(this).attr('id');
+      var hei = $(this).outerHeight();
+      var grttop = $(this).offset().top - 70;
+      if($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+        $(".navbar-nav li a[href='#']" + bb + "']").parent().addClass("active");
+      } else {
+        $(".navbar-nav li a[href='#']" + bb + "']").parent().removeClass("active");
+      }
+    })
+  })
+});
+
+//Add counter
+
+// $(document).ready(function() {
+//   $('.counter-num').counterUp({
+//     delay: 10,
+//     time: 2000,
+//   })
+// })
+
+//Add a wow.js object
+$(document).ready(function(){
+  'use strict'
+  new WOW().init();
 });
