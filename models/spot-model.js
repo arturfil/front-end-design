f50344de1b9e6b250758dc3476b3c69322ee912a
arturfil <arturfil@hotmail.com>
2017-09-21 14:26:03 -0400

    new domain name

--- models/spot-model.js
+++ models/spot-model.js
@@ -6,6 +6,8 @@ const spotSchema = new Schema(
     name:     {type: String, required: true},
     workout:  {type: String, enum: [ 'Cardio', 'Resistance', 'Cardio & Resistance']},//selector in the form
     location: { type: String},
+    latitud:  {type: Number},
+    longitud: {type: Number},
     rating:   {type: Number},
     image:    {type: String}
   }
